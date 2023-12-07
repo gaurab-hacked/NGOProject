257b9c98a682d55b5aba2f6e7ef07d11e8f468e5
Gaurab-sunar <gaurabsuna@gmail.com>
2023-12-07 14:32:55 +0545

    gaurab

--- client/app/(front)/common/Navigation/BottomNav.js
+++ client/app/(front)/common/Navigation/BottomNav.js
@@ -1,9 +1,49 @@
+"use client";
 import React from "react";
+import { Button } from "@nextui-org/react";
+import Link from "next/link";
+// import Donate from "./svg/hand_dollar.svg";
+import { BiSolidDonateHeart } from "react-icons/bi";
+import LOGO from "./newlogo.jpg";
+import Image from "next/image";
 
 const BottomNav = () => {
   return (
     <>
-      <div>BottomNav</div>
+      <div className="w-full h-[60px] bg-white shadow flex items-center justify-between">
+        <div id="logo w-[40%] flex">
+          <Image src={LOGO} className="ml-10" alt="logo" height={55} />
+        </div>
+        <div className="w-[60%]">
+          <ul className="flex h-full items-center w-[95%] justify-between ">
+            <li>
+              <Link href={"/"}>Home</Link>
+            </li>
+            <li>
+              <Link href={"/"}>What We Do</Link>
+            </li>
+            <li>
+              <Link href={"/"}>Projects</Link>
+            </li>
+            <li>
+              <Link href={"/"}>Contact</Link>
+            </li>
+            <li>
+              <Link href={"/"}>News & Events</Link>
+            </li>
+            <li>
+              <Button
+                radius="none"
+                className="text-sm uppercase tracking-wide rounded-sm text-white"
+                color="success"
+                startContent={<BiSolidDonateHeart className="scale-110" />}
+              >
+                Donate
+              </Button>
+            </li>
+          </ul>
+        </div>
+      </div>
     </>
   );
 };
--- client/app/(front)/common/Navigation/TopNav.js
+++ client/app/(front)/common/Navigation/TopNav.js
@@ -1,9 +1,40 @@
+"use client";
 import React from "react";
+import { AiFillFacebook } from "react-icons/ai";
+import { FaPhoneAlt } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
+
+import {
+  FaInstagramSquare,
+  FaLinkedin,
+  FaPinterestSquare,
+} from "react-icons/fa";
 
 const TopNav = () => {
   return (
     <>
-      <div>TopNav</div>
+      <div className="w-full bg-[#08bee3] h-[30px] flex justify-end items-center py-1">
+        <div className="w-[1px] h-full bg-slate-600 mx-3"></div>
+        <div className="flex space-x-3 ml-1">
+          <AiFillFacebook className="text-xl text-black cursor-pointer" />
+          <FaInstagramSquare className="text-xl text-black cursor-pointer" />
+          <FaPinterestSquare className="text-xl text-black cursor-pointer" />
+          <FaLinkedin className="text-xl text-black cursor-pointer" />
+        </div>
+        <div className="w-[1px] h-full bg-slate-600 mx-3"></div>
+        <div className="flex items-center gap-1">
+          <FaPhoneAlt className="scale-90" />
+          <p className=" text-black tracking-wide">+9779810325922</p>
+        </div>
+        <div className="w-[1px] h-full bg-slate-600 mx-3"></div>
+        <div className="flex items-center gap-1 ">
+          <MdEmail className="scale-110 mt-1" />
+          <p className=" text-black tracking-wide">
+            info@digitalempowerment.org
+          </p>
+        </div>
+        <div className="w-[1px] h-full bg-slate-600 mr-4 mx-3"></div>
+      </div>
     </>
   );
 };
