7d3c803f061ea6b455f481f01be567784809ece9
Gaurab-sunar <gaurabsuna@gmail.com>
2023-12-18 19:18:49 +0545

    gaurab

--- client/app/dashboard/layout.js
+++ client/app/dashboard/layout.js
@@ -8,7 +8,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <div>
-      <PageContent children={children} />
+      <PageContent>{children}</PageContent>
     </div>
   );
 }
